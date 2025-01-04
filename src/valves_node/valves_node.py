77d563004ebab0115b096c6ab4502daa40a81f38
amr kasem <amr.kasem@gmail.com>
2025-01-04 12:34:17 -0500

    merged

--- src/valves_node/valves_node.py
+++ src/valves_node/valves_node.py
@@ -49,7 +49,7 @@ class ValveController:
 
     def setup_logger(self):
         logger = logging.getLogger(self.__class__.__name__)
-        self.logger.setLevel(logging.INFO)
+        logger.setLevel(logging.INFO)
         os.makedirs('logs', exist_ok=True) 
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         ch = logging.StreamHandler()
