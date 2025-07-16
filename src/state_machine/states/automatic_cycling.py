a35681419c5e9fbbed7977337e0931ab7f180e8f
device2 <amr.kasem@gmail.com>
2025-07-16 19:02:23 -0400

    merged

--- src/state_machine/states/automatic_cycling.py
+++ src/state_machine/states/automatic_cycling.py
@@ -62,7 +62,7 @@ class AutomaticCyclingState(State):
                             if "POSITIVE_RELEASE" in valve['role']:
                                 self.machine.client.publish(f'{self.machine.device_id}/valves/{valve["name"]}',1) # on // pump
                         
-                        time.sleep(1.3) # modif
+                        time.sleep(1.0) # modif
                         break
                     time.sleep(0.02)
                 
